7ebee65fb746dbe1591044eeb423b243063f0bf9
illuminator3 <hardt-j@web.de>
2021-09-21 15:44:41 +0200

    Javadoc

--- application/src/main/java/org/togetherjava/tjbot/commands/CommandUtility.java
+++ application/src/main/java/org/togetherjava/tjbot/commands/CommandUtility.java
@@ -1,8 +1,22 @@
 package org.togetherjava.tjbot.commands;
 
+/**
+ * Utility class for the command system
+
+ * @author illuminator3
+ */
 public final class CommandUtility {
     private CommandUtility() {}
 
+    /**
+     * Escapes every character in a string that is not a-zA-Z0-9, a space, or \n respectively \r with a backslash (\)<br><br>
+     * Example:<br>
+     * {@code `System.out.println("Hello World")`} would get turned into {@code \`System\.out\.println\(\"Hello World\"\)\`}
+     *
+     * @param s string to escape
+     * @return escaped string
+     * @author illuminator3
+     */
     public static String escape(String s) {
         return s.replaceAll("([^a-zA-Z0-9 \n\r])", "\\\\$1");
     }
